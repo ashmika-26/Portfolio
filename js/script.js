3fbb9ec3d26dd8415c6c61710e737592a1472974
Ashmika <ashmika.b26@gmail.com>
2020-07-06 00:13:35 -0700

    Image changes

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ $(document).ready(function() {
     });
 
     $("#navigation li a").click(function(e) {
-		    e.preventDefault();
+		e.preventDefault();
 		var targetElement = $(this).attr("href");
 		var targetPosition = $(targetElement).offset().top;
 		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
